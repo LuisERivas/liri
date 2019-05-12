1bc93507977bbd4c74192ad6cc54e1f0f10e2362
LuisERivas <lucho.rivas.villacorta@gmail.com>
2019-05-11 23:31:56 -0700

    added function that filters what option was chosen by user

--- liri.js
+++ liri.js
@@ -11,7 +11,7 @@ function askForUserInput(){
         {
             type: `list`,
             message: `What do you want to do?`,
-            choices: [`Search for concerts`,`Search for song on spotify`,`Search for movie information`],
+            choices: [`Search for concerts`,`Search for song on spotify`,`Search for movie information`, `Random`],
             name: `userSelectedTask`
         },
         {
@@ -23,8 +23,33 @@ function askForUserInput(){
     ])
     .then(function(inquirerResponse){
     if(inquirerResponse.confirm){
-        console.log(`----------------------`)
+        console.log(`---------------------------------------`)
         console.log(`you have chosen to: ${inquirerResponse.userSelectedTask}`)
-        }
+        console.log(`---------------------------------------`)
+        var input= inquirerResponse.userSelectedTask
+        userSelectedTaskInput(input)
+
+    }
     })
 }
+
+
+// function that will ask user to input what they are looking for 
+function userSelectedTaskInput(input){
+    if(input=== `Search for concerts`){
+        console.log(`this is testing that you chose Search for Concerts`)
+    }
+    else if(input=== `Search for song on spotify`){
+        console.log(`this is testing that you chose Search for song`)
+    }
+    else if(input=== `Search for movie information`){
+        console.log(`this is testing that you chose Search for movie`)
+    }
+    else if(input==="Random"){
+        console.log(`this is testing that you chose Search for Random`)
+    }
+    else{
+        console.log(`you suck...`)
+    }
+}
+
